76717a74587703fc18d4386e72f5b205ab1c1cbf
beingMayuresh <mayureshgupta@hotmail.com>
2016-11-25 22:39:17 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+  $(document).ready(function() {
+
+	$("#header1").addClass("animated pulse");
+	$("#header2").addClass("animated rubberBand");
+	$("#header3").addClass("animated rubberBand");
+	$("#header4").addClass("animated rubberBand");
+
+  });
